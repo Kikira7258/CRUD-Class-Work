95b7327c478fb312c77dc298ca8bff62ddff51b7
Anita Baker <anita.baker@gmail.com>
2022-05-08 22:26:27 -0500

    https://github.com/Kikira7258/CRUD-Class-Work

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const path = require('path');
 const { postcss } = require('tailwindcss');
 const { response } = require('express');
 const router = express.Router();
+const flash = require('express-flash');
 
 var app = express();
 
@@ -24,20 +25,12 @@ app.use(express.static(path.join(__dirname,'/public')))
 // config express server
 app.use(express.json());
 app.use('/', router);
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', function(req, res) {
     res.redirect('/projects')
 })
-
-// app.get('/add', function(req, res) {
-//     res.render('addnote')
-// })
-
-// app.get('/projects', function(req, res) {
-//     res.render('projects')
-// })
 
 
 var conn = mysql.createConnection({
@@ -48,13 +41,11 @@ var conn = mysql.createConnection({
 })
 
 
-
 // tells if the database is connected or not
 conn.connect(function(err){
     if(err) console.log(err);
     else console.log('Database connected!')
 })
-
 
 
 // Fetching fields from the database
@@ -81,10 +72,12 @@ app.get('/projects', (req, res) => {
 
 
 
-// Send to add
+// ADD ROUTE
+// When adding the fields, it should be clear
 app.get('/add-project', (req, res) => {
             res.render('addproject',
             {
+                project_id: "",
                 project_title: "",
                 project_description: "",
                 project_start_dt: "",
@@ -93,62 +86,60 @@ app.get('/add-project', (req, res) => {
         })
 
 
-        // note
-app.get('/add-note', (req, res) => {
+        // linking the note route dynamically to the project
+app.get('/add-note/:id', (req, res) => {
             res.render('addnote',
             {
-                project_id: "",
+                project_id: req.params.id,
                 note: "",
                 active_date: "",
+                note_id: ""
             })
         })
  
 
+        // getting data from the form
+app.post('/add-note/:id', (req, res) => {
+    let nt_data = {
+        project_id: req.params.id,
+        note: req.body.note,
+        active_date: req.body.active_date
+    }
+
+        // query insertd into the database, then after its directed back to the project
+    conn.query('INSERT INTO notes SET ?', nt_data, (err, results) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.redirect('/project_details/' +  req.params.id)
+        }
+    })
+
+})
 
 
 
 
 
+// corresponse with the name in the html, its also the same when creating, should be the same for codes sharing the same route
 
-// app.get('/projects/:id', (req, res) => {
-//     conn.query('SELECT * FROM projects WHERE id='+ req.params.id, (err, rows, fields) => {
-//         if (!err) {
-//             res.render('projects',
-//             {
-//                 project: [
-//                     {project_title: "title",
-//                     project_description: "description",
-//                     project_start_dt: "start_date",
-//                     project_due_dt: "due_date"}
-//                 ],
-//                 data: rows
-//             })
-//         }
-//         else
-//             console.log(err);
-//     })
-// })
-
-
-
-
-app.post('/add-project', (res, req) => {
+app.post('/add-project', (req, res) => {
     let data = {
         
-        project_title: req.body.project_title,
-        project_description: req.body.project_description,
-        project_start_dt: req.body.project_start_dt,
-        project_due_dt: req.body.project_due_dt
+        project_title: req.body.title,
+        project_description: req.body.description,
+        project_start_dt: req.body.start_date,
+        project_due_dt: req.body.due_date
     };
 
 
 
-    let sqlQuery = "INSERT INTO students SET ?"
+    let sqlQuery = "INSERT INTO projects SET ?"
 
     let vQuery = conn.query(sqlQuery, data, (err, results) => {
         if(err) throw err;
-            res.setEncoding(JSONResponse(results));
-            // res.redirect('/')
+            // res.setEncoding(JSONResponse(results));
+            res.redirect('/projects')
     });
 });
 
@@ -171,25 +162,118 @@ function redirectTo(locationURL){
 
 
 
-// app.post('/add-project', (res, req) => {
-//     let sqlQuery = "UPDATE projects SET project_title ='" + req.body.project_title +
-//                 "', project_description ='" + req.body.project_description +
-//                 "', project_start_dt ='" + req.body.project_start_dt +
-//                 "', project_due_dt =" + req.body.project_due_dt +
-//                 "' WHERE id = " + req.body.id;
-// })
+
+
+// When editing, the fields will be the data from the database
+app.get('/projects/edit/:id', (req, res) => {
+    conn.query('SELECT * FROM projects WHERE id = ' + req.params.id, (err, row) => {
+        if(err){
+            console.log(err)
+        } else {
+            console.log(row[0])
+
+            // dynamically change the fields of the edited project to sync with its project
+            // new Date(<date>).toISOString().split('T')[0]
+
+            res.render('addproject', {
+                project_id: row[0].id,
+                project_title: row[0].project_title,
+                project_description: row[0].project_description,
+                project_start_dt: new Date(row[0].project_start_dt).toISOString().split('T')[0],
+                project_due_dt: new Date(row[0].project_due_dt).toISOString().split('T')[0]  
+                
+            });
+        }
+    })
+})
 
 
 
 
-// app.post('/add-project', (res, req) => {
-//     let sqlQuery = "UPDATE projects SET project_id ='" + req.body.project_id +
-//                 "', note ='" + req.body.note +
-//                 "', active_date ='" + req.body.active_date +
-//                 "' WHERE id = " + req.body.id;
-// })
 
 
+
+// corresponse with the name in the html, its also the same when creating, should be the same for codes sharing the same route
+app.post('/projects/update/:id', (req, res) => {
+    let sqlQuery = "UPDATE projects SET project_title ='" + req.body.title +
+                "', project_description ='" + req.body.description +
+                "', project_start_dt ='" + req.body.start_date +
+                "', project_due_dt ='" + req.body.due_date +
+                "' WHERE id = " + req.params.id;
+
+
+                // set the route to redirect to after post
+                conn.query(sqlQuery, (err, row) => {
+                    if(err) {
+                        console.log(err)
+                    } else {
+                        res.redirect('/projects')                        
+                    }
+                })
+                
+})
+
+
+
+
+
+// Delete project page
+app.get('/projects/delete/:id', function(req, res, next) {
+    conn.query('DELETE FROM projects WHERE id = '+ req.params.id, function(err, row) {
+        if(!err) {
+            res.redirect('/projects')
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+
+
+
+
+
+// Array starts at zero [0]
+// Projects Details
+app.get('/project_details/:id', function(req, res, next) {
+    conn.query('SELECT * FROM projects WHERE id = '+ req.params.id, function(err, row) {
+        if(!err) {
+            conn.query('SELECT * FROM notes WHERE project_id = ' + req.params.id, (err, notes) => {
+                if(!err) {
+                    res.render('project-details', {
+                        project_id: row[0].id,
+                        project_title: row[0].project_title,
+                        project_description: row[0].project_description,
+                        project_start_dt: new Date(row[0].project_start_dt).toISOString().split('T')[0],
+                        project_due_dt: new Date(row[0].project_due_dt).toISOString().split('T')[0],
+                        
+                        data: notes,
+                        
+                    });
+                }
+            })
+            
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+
+
+
+
+// Delete note
+
+app.get('/note/delete/:id/:project_id', function(req, res, next) {
+    conn.query('DELETE FROM notes WHERE id = '+ req.params.id, function(err, row) {
+        if(!err) {
+            res.redirect('/project_details/' + req.params.project_id)
+        } else {
+            console.log(err);
+        }
+    })
+})
 
 
 
